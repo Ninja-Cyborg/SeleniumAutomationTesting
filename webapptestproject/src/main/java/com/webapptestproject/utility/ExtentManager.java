f895cb1326e7fea01786b3e7cbd07018f8767baa
Abhijot <66517017+Ninja-Cyborg@users.noreply.github.com>
2022-12-25 17:18:39 -0800

    Update ExtentManager.java

--- webapptestproject/src/main/java/com/webapptestproject/utility/ExtentManager.java
+++ webapptestproject/src/main/java/com/webapptestproject/utility/ExtentManager.java
@@ -13,16 +13,12 @@ public class ExtentManager {
 	public static ExtentTest test;
 	
 	public static void setExtent() throws IOException {
-		//htmlReporter= new ExtentHtmlReporter(System.getProperty("user.dir")+"/test-output/ExtentReport/"+"MyReport_"+BaseClass.getCurrentTime()+".html");
 		sparkReporter= new ExtentSparkReporter(System.getProperty("user.dir")+"/test-output/ExtentReport/"+"report.html");
 		sparkReporter.loadXMLConfig(System.getProperty("user.dir")+"/extent-config.xml");
-		//htmlReporter.config().setDocumentTitle("Automation Test Report");
-		//htmlReporter.config().setReportName("OrangeHRM Test Automation Report");
-		//htmlReporter.config().setTheme(Theme.DARK);
 		
 		extent = new ExtentReports();
-		extent.attachReporter(sparkReporter);
 		
+		extent.attachReporter(sparkReporter);
 		extent.setSystemInfo("HostName", "rootUser");
 		extent.setSystemInfo("ProjectName", "SeleniumTestingProject");
 		extent.setSystemInfo("Tester", "Selenium");
